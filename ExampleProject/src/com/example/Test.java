cbf800bad8871ab35608999d21e168db0dff62a6
Satya <Satya@Satya-PC>
2018-06-17 22:57:30 +0530

    old1

--- ExampleProject/src/com/example/Test.java
+++ ExampleProject/src/com/example/Test.java
@@ -4,8 +4,8 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("First second Project");
-		System.out.println("second change");
+		System.out.println("old1");
+		
 	}
 
 }
