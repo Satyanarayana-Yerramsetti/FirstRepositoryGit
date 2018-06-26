b4bc9d25f559ed1d5d1b1c819aaeb443cd85678e
Satya <Satya@Satya-PC>
2018-06-26 22:44:09 +0530

    git2

--- ExampleProject/src/com/example/Test.java
+++ ExampleProject/src/com/example/Test.java
@@ -5,8 +5,5 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("git2---1");
-		System.out.println("git2---2");
-		System.out.println("git2---3");
 	}
-
 }
