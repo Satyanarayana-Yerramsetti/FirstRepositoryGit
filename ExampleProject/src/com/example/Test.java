8957192eddcb77b20a236e610ad835d6f1309c0c
Satya <Satya@Satya-PC>
2019-01-30 00:32:48 +0530

    First Eclipse commit

--- ExampleProject/src/com/example/Test.java
+++ ExampleProject/src/com/example/Test.java
@@ -8,5 +8,11 @@ public class Test {
 		System.out.println("eclipse 1");
 		System.out.println("Eclipse 2");
 		System.out.println("Second brach changes for merging");
+		
+		//from first eclipse
+		System.out.println("Second Branch");
+		System.out.println("eclipse 1");
+		System.out.println("Eclipse 2");
+		System.out.println("Second brach changes for merging");
 	}
 }
