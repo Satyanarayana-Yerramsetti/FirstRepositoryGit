ba3cfcdf67882e505efe9edb72ff758f26a07a21
Satya <Satya@Satya-PC>
2019-01-29 23:55:36 +0530

    #Comment First commit from eclipse 1

--- ExampleProject/src/com/example/Test.java
+++ ExampleProject/src/com/example/Test.java
@@ -5,18 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("new1");
-		System.out.println("old1");
-		System.out.println("new2");
-		System.out.println("new1");
-		System.out.println("old2");
-		System.out.println("git2---1");
-		System.out.println("git2---2");
-		System.out.println("git3---2");
-		System.out.println("narayana");
-		System.out.println("satya");
-		System.out.println("Srinu");
-		System.out.println("nagamani");
+		System.out.println("eclipse 1");
 		
 		
 	}
